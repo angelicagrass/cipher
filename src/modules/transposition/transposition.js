6fa6c6cfdff0b9148dcb609ae36e1dc3941f765c
Angelica Grass <ag223vg@student.lnu.se>
2021-11-20 07:59:55 +0100

    move trensposition

---
+++ src/modules/transposition/transposition.js
@@ -0,0 +1,56 @@
+export default class Transposition {
+  constructor(file, encryptOrDecrypt, key) {
+    this.file = file;
+    this.key = key.toString()
+    this.chiperText = ''
+    this.encryptOrDecrypt = encryptOrDecrypt;
+    this.transpositionCipher = this.transpositionCipher() 
+  } 
+
+  transpositionCipher() {
+    for (let i = 0; i < this.file.length; i = i + this.key.length) {     
+      this.moveCharactersInString(this.file.substring(i, i + this.key.length), this.key)
+    }
+    console.log(this.chiperText)
+    return this.chiperText
+  }
+
+  moveCharactersInString(string, key) {
+    if (string.length < key.length) { string += ' '.repeat(key.length - string.length) }
+    for (let j = 0; j < key.length; j++) {
+      let index = Number(key.substring(j, j + 1)) - 1
+      if (this.encryptOrDecrypt === 'e') { this.chiperText += string[index] } 
+        else if (this.encryptOrDecrypt === 'd') { this.chiperText += string.charAt(parseInt(key.charAt(index)) - 1) } 
+    }
+  }
+
+    // encryptDecrypt() {
+  //   if (this.encryptOrDecrypt === 'e') { return this.encryptTranspositionCipher() } 
+  //   else if (this.encryptOrDecrypt === 'd') { return this.encryptTranspositionCipher() }
+  // }
+
+  // test(string, key) {
+  //   if (string.length < key.length) { string += ' '.repeat(key.length - string.length)}
+  //   for (let j = 0; j < key.length; j++) {
+  //     let index = Number(key.substring(j, j + 1)) - 1
+  //     this.chiperText += string[index]
+  //   }
+  // }
+
+  // decrypt transposition cipher
+//   decryptTranspositionCipher() {
+//     let str = ''
+//     for (let i = 0; i < this.file.length; i = i + this.key.length) {
+//       str = this.decrypt(this.file.substring(i, i + this.key.length), this.key) // this.decrypt
+//     }
+// console.log(this.chiperText)
+//     return this.chiperText
+//   }
+//   decrypt(string, key) {
+//     if (string.length < key.length) { string += ' '.repeat(key.length - string.length)}
+//     for (let j = 0; j < key.length; j++) {
+//       let index = Number(key.substring(j, j + 1)) - 1
+//       this.chiperText += string.charAt(parseInt(key.charAt(index)) - 1) // skiljer sig
+//     }
+//   }
+}
