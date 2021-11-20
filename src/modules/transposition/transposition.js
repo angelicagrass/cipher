4fba68116ed7df1fe2f3d591f8ac53d8b5e9632d
Angelica Grass <ag223vg@student.lnu.se>
2021-11-20 09:07:55 +0100

    change word ciphertext

--- src/modules/transposition/transposition.js
+++ src/modules/transposition/transposition.js
@@ -2,7 +2,7 @@ export default class Transposition {
   constructor(file, encryptOrDecrypt, key) {
     this.file = file;
     this.key = key.toString()
-    this.chiperText = ''
+    this.ciphertext = ''
     this.encryptOrDecrypt = encryptOrDecrypt;
     this.transpositionCipher = this.transpositionCipher() 
   } 
@@ -11,16 +11,16 @@ export default class Transposition {
     for (let i = 0; i < this.file.length; i = i + this.key.length) {     
       this.moveCharactersInString(this.file.substring(i, i + this.key.length), this.key)
     }
-    console.log(this.chiperText)
-    return this.chiperText
+    console.log(this.ciphertext)
+    return this.ciphertext
   }
 
   moveCharactersInString(string, key) {
     if (string.length < key.length) { string += ' '.repeat(key.length - string.length) }
     for (let j = 0; j < key.length; j++) {
       let index = Number(key.substring(j, j + 1)) - 1
-      if (this.encryptOrDecrypt === 'e') { this.chiperText += string[index] } 
-        else if (this.encryptOrDecrypt === 'd') { this.chiperText += string.charAt(parseInt(key.charAt(index)) - 1) } 
+      if (this.encryptOrDecrypt === 'e') { this.ciphertext += string[index] } 
+        else if (this.encryptOrDecrypt === 'd') { this.ciphertext += string.charAt(parseInt(key.charAt(index)) - 1) } 
     }
   }
 
@@ -33,7 +33,7 @@ export default class Transposition {
   //   if (string.length < key.length) { string += ' '.repeat(key.length - string.length)}
   //   for (let j = 0; j < key.length; j++) {
   //     let index = Number(key.substring(j, j + 1)) - 1
-  //     this.chiperText += string[index]
+  //     this.ciphertext += string[index]
   //   }
   // }
 
